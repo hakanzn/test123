36a5e7ad34019132099885a4760198be1677f350
hakanzn <hakanzn@gmail.com>
2022-01-12 02:17:52 +0300

    Updates

---
+++ precache-manifest.08254f0e28efb90da29bada8c8e26a6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "75db3712e0826eb6eb7c",
+    "url": "/test123/static/js/runtime~main.75db3712.js"
+  },
+  {
+    "revision": "146ce96e6e8c731dd217",
+    "url": "/test123/static/js/main.146ce96e.chunk.js"
+  },
+  {
+    "revision": "8032f890178ca410313a",
+    "url": "/test123/static/js/1.8032f890.chunk.js"
+  },
+  {
+    "revision": "146ce96e6e8c731dd217",
+    "url": "/test123/static/css/main.b017674f.chunk.css"
+  },
+  {
+    "revision": "02a0f046da4ed34dc6bf501219fadf96",
+    "url": "/test123/index.html"
+  }
+];
