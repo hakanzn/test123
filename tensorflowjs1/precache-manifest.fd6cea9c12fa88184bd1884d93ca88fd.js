973c589bcb00dc36235a770c841e6fe0cb82c8c3
hakanzn <hakanzn@gmail.com>
2022-01-17 21:26:57 +0300

    ilk yükleme
    test

---
+++ tensorflowjs1/precache-manifest.fd6cea9c12fa88184bd1884d93ca88fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "11361b57ee4b17bf3137",
+    "url": "/tensorflowjs1/static/js/runtime~main.11361b57.js"
+  },
+  {
+    "revision": "f249fa9dcf375986b243",
+    "url": "/tensorflowjs1/static/js/main.f249fa9d.chunk.js"
+  },
+  {
+    "revision": "28566c3e2d8e22301698",
+    "url": "/tensorflowjs1/static/js/1.28566c3e.chunk.js"
+  },
+  {
+    "revision": "f249fa9dcf375986b243",
+    "url": "/tensorflowjs1/static/css/main.a77c052b.chunk.css"
+  },
+  {
+    "revision": "be3ce1bf31831d0cace4a0870474bd41",
+    "url": "/tensorflowjs1/index.html"
+  }
+];
